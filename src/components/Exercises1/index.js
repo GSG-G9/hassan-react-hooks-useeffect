d88efc25bc0d499038d6dcbbe0baf5348c20ad4d
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-01-19 15:29:35 +0200

    ex1-solution

--- src/components/Exercises1/index.js
+++ src/components/Exercises1/index.js
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 export default function Exercises1() {
+	const [count, setCount] = useState(0);
+	const incrementCount = () => {
+		setCount((ct) => ct + 1);
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', incrementCount);
+		return () => {
+			document.removeEventListener('mousedown', incrementCount);
+		};
+	}, [count]);
 	return (
 		<div className='exercises-container'>
 			<Link className='button' to='/'>
 				Home
 			</Link>
+			<p>{count}</p>
+			<div>
+				<button className='button'>Add</button>
+			</div>
 		</div>
 	);
 }
