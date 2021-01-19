10e14a21676baa8bc0cca3a960f71bbb8ab725d4
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-01-19 15:22:35 +0200

    ex2-solution

--- src/components/Exercises3/index.js
+++ src/components/Exercises3/index.js
@@ -1,27 +1,28 @@
 import { Link } from 'react-router-dom';
-import { Promise } from 'bluebird';
 import './style.css';
 import { useState, useEffect } from 'react';
-Promise.config({ cancellation: true });
 export default function Exercises4() {
 	const [inputText, setInputText] = useState('code');
 	const [imgs, setImgs] = useState([]);
 
 	useEffect(() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
 		const fetchUser = (queryText) =>
 			new Promise((resolve, reject) => {
 				fetch(
-					`https://api.giphy.com/v1/gifs/search?api_key=f9d7wzeXCo0Xy18AllwGubAe4VXYqf4w&q=${queryText}&limit=25&offset=0&rating=g&lang=en`
+					`https://api.giphy.com/v1/gifs/search?api_key=f9d7wzeXCo0Xy18AllwGubAe4VXYqf4w&q=${queryText}&limit=25&offset=0&rating=g&lang=en`,
+					{ signal }
 				)
 					.then((res) => res.json())
 					.then(({ data }) => resolve(data))
 					.catch((err) => reject(err));
 			});
-		const promise = fetchUser(inputText)
+		fetchUser(inputText)
 			.then((data) => setImgs(data))
 			.catch(() => setImgs([]));
 		return () => {
-			promise.cancel();
+			controller.abort();
 			setImgs([]);
 		};
 	}, [inputText]);
